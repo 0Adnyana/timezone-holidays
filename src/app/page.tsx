f93056e94b76de93b6e54babc7ba750dd1dabdaf
0Adnyana <87359319+0Adnyana@users.noreply.github.com>
2024-08-28 15:01:05 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-"use client";
+import TestNagerDateAPI from "@/components/partials/TestNagerDateAPI";
+import TestPhotonAPI from "@/components/partials/TestPhotonAPI";
+import TestTimeAPI from "@/components/partials/TestTimeAPI";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="flex flex-col"></div>
+			<div className="flex flex-col space-y-4">
+				<TestNagerDateAPI></TestNagerDateAPI>
+				<TestPhotonAPI></TestPhotonAPI>
+				<TestTimeAPI></TestTimeAPI>
+			</div>
 		</main>
 	);
 }
